a3d613ce196399369b77895325b77c8269f72600
kaitlynchan <ununoctium829@gmail.com>
2016-08-26 08:35:49 -0700

    8/26 Hello world demonstration
    yayyy

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class Hello_world here.
+ * program displays hello world to console
+ * @author Kaitlyn Chan
+ * @version 8/26/16
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Kaitlyn"); //Display the string
+    }
+}
+
